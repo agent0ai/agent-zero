4a80438d85bd95ebf71a379bc8182354bf8bf271
mahoosuc-solutions <aaron@mahoosuc.solutions>
2026-02-01 12:33:29 -0500

    feat: initialize Next.js project with Vercel configuration for Agent Jumbo

---
+++ web/next.config.js
@@ -0,0 +1,24 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
+})
