441c0104f9f5d06807f3d50fa8dd2d7bf5a5cfa6
John Kruse <john@kruser.org>
2026-03-18 09:10:02 +0100

    Fail open when memory recall task errors

--- python/extensions/message_loop_prompts_after/_91_recall_wait.py
+++ python/extensions/message_loop_prompts_after/_91_recall_wait.py
@@ -2,7 +2,7 @@ from python.helpers.extension import Extension
 from agent import LoopData
 from python.extensions.message_loop_prompts_after._50_recall_memories import DATA_NAME_TASK as DATA_NAME_TASK_MEMORIES, DATA_NAME_ITER as DATA_NAME_ITER_MEMORIES
 # from python.extensions.message_loop_prompts_after._51_recall_solutions import DATA_NAME_TASK as DATA_NAME_TASK_SOLUTIONS
-from python.helpers import settings
+from python.helpers import settings, errors
 
 class RecallWait(Extension):
     async def execute(self, loop_data: LoopData = LoopData(), **kwargs):
@@ -23,10 +23,16 @@ class RecallWait(Extension):
                     return
             
             # otherwise await the task
-            await task
+            try:
+                await task
+            except Exception as e:
+                self.agent.context.log.log(
+                    type="warning",
+                    heading="Recall memories skipped",
+                    content=errors.format_error(e),
+                )
 
         # task = self.agent.get_data(DATA_NAME_TASK_SOLUTIONS)
         # if task and not task.done():
         #     # self.agent.context.log.set_progress("Recalling solutions...")
         #     await task
-
